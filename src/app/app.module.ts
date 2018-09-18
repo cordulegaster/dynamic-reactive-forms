420accdb1e852d31b30ff5672e9496dadf5c7080
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2018-09-18 20:19:30 +0200

    Step 1: App setup + the register-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { CompanyComponent } from './register/company/company.component';
+import { VatComponent } from './register/company/vat/vat.component';
+import { PersonComponent } from './register/person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    CompanyComponent,
+    VatComponent,
+    PersonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
