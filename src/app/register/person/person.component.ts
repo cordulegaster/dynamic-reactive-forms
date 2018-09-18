393fd24ec3c7c12671fb71418d213d9def7f9cc9
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2018-09-18 20:29:48 +0200

    Step 2: Add the PersonComponent

--- src/app/register/person/person.component.ts
+++ src/app/register/person/person.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-person',
   templateUrl: './person.component.html',
-  styleUrls: ['./person.component.css']
 })
-export class PersonComponent implements OnInit {
-
-  constructor() { }
-
+export class PersonComponent implements OnInit, OnDestroy {
+  @Input('group') registrationForm: FormGroup;
+  personForm = this.fb.group({
+    name: [""],
+    address: [""],
+    age: [""]
+  });
+  constructor(protected fb: FormBuilder) { }
   ngOnInit() {
+    // Register the companyForm when the component initializes.
+    this.registrationForm.addControl("person", this.personForm);
   }
-
+  ngOnDestroy() {
+    // Unregister the companyForm when the component destroys.
+    this.registrationForm.removeControl("person");
+  }
 }
