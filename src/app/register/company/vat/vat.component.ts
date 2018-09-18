0460e62521038b3fb761da7d026ef072d5cfac71
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2018-09-18 20:33:03 +0200

    Step 3: CompanyComponent and VatComponent

--- src/app/register/company/vat/vat.component.ts
+++ src/app/register/company/vat/vat.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { FormGroup, FormBuilder } from "@angular/forms";
 @Component({
   selector: 'app-vat',
   templateUrl: './vat.component.html',
-  styleUrls: ['./vat.component.css']
 })
-export class VatComponent implements OnInit {
-
-  constructor() { }
-
+export class VatComponent implements OnInit, OnDestroy {
+  @Input("group") companyForm: FormGroup;
+  vat = this.fb.group({
+    vatNumber: [""],
+  });
+  constructor(protected fb: FormBuilder) {}
   ngOnInit() {
+    this.companyForm.addControl("vat", this.vat);
   }
-
+  ngOnDestroy() {
+    this.companyForm.removeControl("vat");
+  }
 }
